cfe5c5fd7d6b5c769765a5c009b9c0a49095c551
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-09-08 17:54:48 -0700

    adding  2 c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return : always 0
+ * Return: always 0
  */
  
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * always returns 0
  *
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return(0);
 }
