52208d57f0d700d6a0ac71acd46b4eb8d849bba9
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-09-22 13:02:58 -0700

    Pointers and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - Funtion string.
+ * @dest: destiny value.
+ * @src: String value.
+ * Return: dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, src_lnt, dest_lnt;
+/* src 12 */
+	src_lnt = 0;
+		for (i = 0; src[i] != '\0'; i++)
+			src_lnt++;
+/* dest 1 */
+	dest_lnt = 0;
+		for (i = 0; dest[i] != '\0'; i++)
+			dest_lnt++;
+/* print dest */
+		for (i = 0; i <= src_lnt; i++)
+			dest[dest_lnt + i] = src[i];
+
+
+		return (dest);
+
+}
