86f0a57e9bcb7c8da6cb12f132820d2abcc47263
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-09-29 09:14:30 -0700

    tasks complete

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _sqrt_recursion - Call the function that calculates the sqrt
+  * @n: Value
+  * Return: Value
+  */
+int _sqrt_recursion(int n)
+{
+
+	return (find_sqrt(0, n));
+
+}
+/**
+  * find_sqrt - Calculates the sqrt of a number
+  * @i: Value
+  * @n: Value
+  * Return: -1.
+  */
+
+int find_sqrt(int i, int n)
+{
+
+	if (i * i == n)
+		return (i);
+	else if (i < n)
+		{
+		return (find_sqrt(i + 1, n));
+		}
+	else
+		return (-1);
+
+}
