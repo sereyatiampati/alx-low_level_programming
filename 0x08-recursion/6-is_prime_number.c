dda50a78fa998a60e1d8e6821886922be7784e07
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-09-29 09:27:42 -0700

    tasks done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,54 +1,32 @@
 #include "main.h"
+
 /**
- * is_palindrome - Scroll string is a palindrome.
- * @s: *s.
- * Return: 1 palindrome and 0.
+ * helper - works every time
+ * @i: helper int
+ * @n: integer to test
+ * Return: value
  */
-int is_palindrome(char *s)
+
+int helper(int i, int n)
 {
-
-	int length;
-
-	if (s[0] == '\0')
-		return (1);
-	length = _recursion(s);
-	return (check_palindrome(s, length, 0, length - 1));
-
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
+		return (helper(i + 1, n));
+	return (1);
 }
 
 /**
- * check_palindrome - Value recursion.
- * @s: *s
- * @length: String
- * @s1: First value.
- * @s2: Second value.
- * Return: 1 palindrome and 0.
+ * is_prime_number - is prime or not
+ * @n: integer to compare
+ * Return: bool
  */
-int check_palindrome(char *s, int length, int s1, int s2)
+
+int is_prime_number(int n)
 {
+	int i = 2;
 
-	if (s1 >= s2)
-		return (1);
-
-	if (s[s1] != s[s2])
+	if (n < 2)
 		return (0);
-
-	else
-		return (check_palindrome(s, length, s1 + 1, s2 - 1));
-
-}
-/**
- * _recursion - String lnt recursion.
- * @s: Value string.
- * Return: String.
- */
-int _recursion(char *s)
-{
-
-	if (s[0] != '\0')
-
-		return (1 + _recursion(s + 1));
-	else
-		return (0);
-
+	return (helper(i, n));
 }
