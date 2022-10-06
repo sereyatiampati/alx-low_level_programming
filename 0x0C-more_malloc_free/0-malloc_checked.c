c7bff7dd33834f3129308721b5bf7c088cd72c15
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-06 10:05:43 -0700

    Malloc is quite something

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: Amount memory the array.
+ * Return: Allocate pointer in memory.
+*/
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	exit(98);
+	return (p);
+
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - Function string.
+ * @s: Value string check.
+ * Return: String.
+ */
+int _strlen(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+		return (i);
+
+}
+/**
+ * string_nconcat - Concatenate two strings.
+ * @s1: My first string.
+ * @s2: My second string.
+ * @n: NUmber bytes from s2 use.
+ * Return: cc.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	unsigned int i, len1, len2;
+	char *cc;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n > len2)
+		n = len2;
+
+	cc = malloc(sizeof(char) * (len1 + n + 1));
+	if (cc == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		cc[i] = s1[i];
+	for (; i < len1 + n; i++)
+		cc[i] = s2[i - len1];
+	cc[len1 + n] = '\0';
+
+	return (cc);
+
+}
