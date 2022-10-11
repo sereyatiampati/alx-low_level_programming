63711d272afb8c8c6f677d4339d1898b043eca5c
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-11 15:35:20 -0700

    dog task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef Macro_dog
+#define Macro_dog
+/**
+ * struct dog - Elementos of dog.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - New elements dog.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+ * @d: data.
+*/
+#endif 
