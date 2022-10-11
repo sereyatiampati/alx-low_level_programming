7efac8e6194fbfd908635272cb2ec1bec4e16aeb
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-11 12:19:50 -0700

    macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef Macro_like_value
-#define Macro_like_value
+#ifndef funtion_like_macro
+#define funtion_like_macro
 #define ABS(x) ((X < 0) ? -(X) : (X))
 #endif /*Macro funtion like macro*/
