6ae70a25ca86ef8583f74ef7cf8b337dfa72797b
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-11 16:07:31 -0700

    dog task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <dog.h>
-
+/**
+ * init_dog - Elementos of dog.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+ * @d: Value name variable.
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 { 
 	if (d)
