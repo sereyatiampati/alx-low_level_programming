36ae45ab3438c7cc780f4ea0e628a524a8333e1d
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-11 12:25:40 -0700

    macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef funtion_like_macro
 #define funtion_like_macro
-#define ABS(x) ((X < 0) ? -(X) : (X))
+#define ABS(X) ((X < 0) ? -(X) : (X))
 #endif /*Macro funtion like macro*/
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef Macro_sum_value
+#define Macro_sum_value
+#define SUM(X, Y) ((X) + (Y))
+#endif /*Macro funtion SUM*/
