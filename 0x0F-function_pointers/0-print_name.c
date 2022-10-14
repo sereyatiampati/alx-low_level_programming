f52cdd56e97ed7ffaae660739b1b1c53e37d1ce6
Emilly <sereyatiampatistudiesgit config --global user.email sereyatiampatistudies>
2022-10-13 17:52:53 -0700

    hey functions and pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,6 @@
  * print_name - Print a name.
  * @name: The name to print.
  * @f: Pointer the funtion.
- * Return: 0;
  */
 void print_name(char *name, void (*f)(char *))
 {
@@ -13,5 +12,4 @@ void print_name(char *name, void (*f)(char *))
 	if (f == NULL)
 		return;
 		(*f)(name);
-
 }
